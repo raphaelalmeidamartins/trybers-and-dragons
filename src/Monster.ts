5159c68aa3066cf6ce8677e2637bb4cf641c6e83
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-08-09 22:47:16 -0300

    :sparkles: feat: create Monster class

---
+++ src/Monster.ts
@@ -0,0 +1,51 @@
+import { SimpleFighter } from './Fighter';
+import getRandomInt from './utils';
+
+class Monster implements SimpleFighter {
+  private _minLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._minLifePoints = -1;
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  private static randomValue(): number {
+    return getRandomInt(1, 10);
+  }
+
+  get minLifePoints(): number {
+    return this._minLifePoints;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  private set decreaseLifePoints(damage: number) {
+    if (damage > 0) {
+      let updatedValue = this.lifePoints - damage;
+      updatedValue = updatedValue <= this.minLifePoints
+        ? this.minLifePoints
+        : updatedValue;
+      this._lifePoints = updatedValue;
+    }
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    this.decreaseLifePoints = attackPoints;
+    return this.lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this.strength);
+  }
+}
+
+export default Monster;
